9006ab085a1df7fd6566e97aa9bb2bea3c13e6c7
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-10 23:40:49 +0530

    variable defining methods

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,3 +14,12 @@ accountCity  = "delhi";
 
 console.log(accountId);
 console.table([accountId , accountEmail ,accountPass , accountCity , accountState]);
+
+
+
+// Const => oce declared and assigned then can't be changed thorut the program.
+//  Var => can be changed after declaration but didn't have scopeing methond like it;s is declared for the whole program .
+//  Let =>  can be changed after declaration and have scopeing methods like it;s is declared for the whole program .
+
+// Reccomended to use let  and const for declaration as var is outdated .git pull origin main
+
