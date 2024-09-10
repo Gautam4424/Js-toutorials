8c26ccc9fffa0c19f9eddbf7479a2b8d8bb3a798
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-11 02:03:33 +0530

    conversion of data types and operator precedence

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -31,6 +31,54 @@
 // "any value" => true
 
 
-let  someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber)
+// let  someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber)
+
+
+
+
+// ************************************Operations***************************************
+//  let value =3;
+//  let negValue= -value
+//  console.log(negValue)
+
+//  console.log(2+2); // addition
+//  console.log(2-2);  // Subtraction
+//  console.log(2*2);  // multiply
+//  console.log(2**3); // 2 raise to the power 3 
+//  console.log(2%2); // 2%2 gives the reminder
+//  console.log(2/2); // gives the quotient 
+
+
+
+// ********************* concatination *************************
+
+//  let str1  ="hello";
+//  let str2 = " Gautam";
+
+//  let str3 = str1 + str2 ;
+
+//  console.log(str3);
+
+
+// Operator precedence
+
+// console.log("1" + 2); //12
+// console.log (1  + "2"); //12
+// console.log("1" + 2 + 2) //  first 1+2 get 12 and type will be string then another 2 get add in it and the type is still string
+// console.log(1+2 + "2") // 1+2 = 3 then add into string and converted into string 32
+
+
+
+
+
+// Prefix and postfix operator  ++i (prefix)  i++  (postfix)
+let gameCounter=100;
+
+console.log(gameCounter++); //100 the increase by 1
+console.log(gameCounter); // print the increased value
+
+
+let gameCounter2=100;
+console.log(++gameCounter2) // 101 will print as first it'll increment and then it will print 
