d2b2bea2fc0de732454b78c5d9308929af52f972
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-11 00:43:14 +0530

    changes in files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "gautamsachdeva2345@gmail.com";
+var accountPass = "12345";
+accountCity = "Jaipur";
+
+let accountState ;
+
+
+// accountId =2;
+
+accountEmail = "hx@hc.com";
+accountPass = "1234567898765";
+accountCity  = "delhi";
+
+console.log(accountId);
+console.table([accountId , accountEmail ,accountPass , accountCity , accountState]);
+
+
+
+// Const => oce declared and assigned then can't be changed thorut the program.
+//  Var => can be changed after declaration but didn't have scopeing methond like it;s is declared for the whole program .
+//  Let =>  can be changed after declaration and have scopeing methods like it;s is declared for the whole program .
+
+// Reccomended to use let  and const for declaration as var is outdated .git pull origin main
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all js code to newer version like latest version it is a defaultlyy enabled 
+
+// alert ("hello");  use in developer console like in google browser and index.html it is avaialaible is js but not is node js 
+
+console.log(3 
+    +3)   // code readibility should be high not like this .
+console.log("hello")
+
+
+let name ="Gautam" // String data type
+let age = 20  //number
+let isLoggedIn =  true  // boolean (true or false)
+
+
+
+//  Premetive datatypes = 
+
+//{ Number range  =>  2 to the power of 53 
+// Number which is greaster that number then we use bigint
+// Boolean  = true or flase 
+// null => Standalone value 
+// Undefined =>  values is not defined but declared 
+// symbol => unique }
+
+
+//  Object
+
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(null))// object
