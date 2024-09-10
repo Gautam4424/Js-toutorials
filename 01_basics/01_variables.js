ba4c4c844145a9687a6ea4c9fa49acdfbfad8c59
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-10 23:33:27 +0530

    Types of delareation methods in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "gautamsachdeva2345@gmail.com";
+var accountPass = "12345";
+accountCity = "Jaipur";
+
+let accountState ;
+
+
+// accountId =2;
+
+accountEmail = "hx@hc.com";
+accountPass = "1234567898765";
+accountCity  = "delhi";
+
+console.log(accountId);
+console.table([accountId , accountEmail ,accountPass , accountCity , accountState]);
