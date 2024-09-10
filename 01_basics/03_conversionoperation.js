b9cd18da0683126f219dd1ee5bc8f95c13244e95
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-11 01:16:55 +0530

    conversion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,36 @@
+// let score =null ; 
+
+// // Both are correct 
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// // Type conversion string to number 
+// let valuInNumber = Number (score);
+// console.log(typeof(valuInNumber)); // Number
+// console.log(valuInNumber) //Nan (not a number)
+
+
+// "33" => 33
+//  "33abc" => Nan  (type of nan is number itself)
+//  true => 1
+//  false  => 0
+//  null => 0
+//  undefined => nan
+
+
+
+//  checking 1 in true and 0 is false 
+
+
+// let isLoggedIn = 0;
+// let booleanChange = Boolean(isLoggedIn);
+// console.log(booleanChange);
+
+// 1 => true  ; 0=>false
+//  "" => false 
+// "any value" => true
+
+
+let  someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
