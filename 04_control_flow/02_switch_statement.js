c8506be0a21bdbd0ba846af75498e4899a15ed1f
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-10-26 00:12:07 +0530

    control flow statements

---
+++ 04_control_flow/02_switch_statement.js
@@ -0,0 +1,72 @@
+
+// Syntax
+
+// switch (key) {
+//     case value:   
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 1;
+
+switch(month){
+    case 1: {
+        console.log("January");
+        break;
+    }
+    case 2: {
+        console.log("Feb");
+        break;
+    }
+    case 3: {
+        console.log("Mar");
+        break;
+    }
+    case 4: {
+        console.log("April");
+        break;
+    }
+    case 5: {
+        console.log("May");
+        break;
+    }
+    case 6: {
+        console.log("June");
+        break;
+    }
+    case 7: {
+        console.log("July");
+        break;
+    }
+    case 8: {
+        console.log("Aug");
+        break;
+    }
+    case 9: {
+        console.log("sept");
+        break;
+    }
+    case 10: {
+        console.log("oct");
+        break;
+    }
+    case 11: {
+        console.log("nov");
+        break;
+    }
+    case 12: {
+        console.log("dec");
+        break;
+    }
+
+    default : {
+        console.log("Not a month")
+        break
+    }
+
+    
+}
+
---
+++ 04_control_flow/03_truly_falsy_satement.js
@@ -0,0 +1,23 @@
+// // falsy value 
+
+
+// // false 
+// // 0
+// // -0
+// // BigInt 0n 
+// // ""
+// // null
+// // undefined
+// // NAN
+
+
+// // other that this all the value are considered as true values 
+
+// // true special values
+
+// "0"  //this is true 
+// 'false'
+// " " //this is also true
+// []
+// {}
+// function(){} //this functiion is know as empty function and ths function has true value 
